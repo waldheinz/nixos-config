ed0594355ec90dace00827391b8fb31d50f02b13
Matthias Treydte <mt@waldheinz.de>
2019-09-17 18:33:49 +0200

    machine/gehirnfasching: enable sysctl modules (and fix indentation)

--- machines/gehirnfasching/configuration.nix
+++ machines/gehirnfasching/configuration.nix
@@ -1,53 +1,43 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ ./hardware-configuration.nix
-      ../../modules/basics.nix
-      ../../modules/fonts.nix
-      ../../modules/i18n.nix
-      ../../modules/networking.nix
-      ../../modules/nfs-blackbox.nix
-      ../../modules/sway.nix
-      ../../modules/syncthing.nix
-      ../../modules/sysadmin.nix
-      ../../modules/users.nix
-      ../../modules/x11.nix
-      ../../modules/zfs.nix
-      ../../modules/zsh.nix
+    imports = [
+        ./hardware-configuration.nix
+        ../../modules/basics.nix
+        ../../modules/fonts.nix
+        ../../modules/i18n.nix
+        ../../modules/networking.nix
+        ../../modules/nfs-blackbox.nix
+        ../../modules/sway.nix
+        ../../modules/syncthing.nix
+        ../../modules/sysadmin.nix
+        ../../modules/sysctl.nix
+        ../../modules/users.nix
+        ../../modules/x11.nix
+        ../../modules/zfs.nix
+        ../../modules/zsh.nix
     ];
 
-  nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.allowUnfree = true;
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.supportedFilesystems = [ "zfs" ];
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.supportedFilesystems = [ "zfs" ];
 
-  networking.hostName = "gehirnfasching"; # Define your hostname.
-  networking.hostId = "e45f2a2c";
+    networking.hostName = "gehirnfasching"; # Define your hostname.
+    networking.hostId = "e45f2a2c";
 
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
+    # Set your time zone.
+    time.timeZone = "Europe/Berlin";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+    # Enable sound.
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.03"; # Did you read the comment?
-
+    # This value determines the NixOS release with which your system is to be
+    # compatible, in order to avoid breaking some software such as database
+    # servers. You should change this only after NixOS release notes say you
+    # should.
+    system.stateVersion = "19.03"; # Did you read the comment?
 }

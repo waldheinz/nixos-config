61deea505b9170d305cb366205f0013cdf5172e7
Matthias Treydte <mt@waldheinz.de>
2019-09-19 21:50:10 +0200

    machine/blackbox/prometheus: scrape router, too

--- machines/blackbox/prometheus.nix
+++ machines/blackbox/prometheus.nix
@@ -4,17 +4,31 @@
   services.prometheus = {
     enable = true;
     extraFlags = [ "--storage.tsdb.retention.time 10y" ];
+
+    globalConfig = {
+      scrape_interval = "30s";
+    };
+
     scrapeConfigs = [
       {
         job_name = "homematic";
         scrape_interval = "60s";
-	metrics_path = "/addons/webmatic/cgi/prometheus.cgi";
-	static_configs = [
-	  {
+        metrics_path = "/addons/webmatic/cgi/prometheus.cgi";
+        static_configs = [
+          {
             targets = [ "ccu.lan.waldheinz.de:80" ];
-	  }
-	];
+          }
+        ];
+      }
+      {
+        job_name = "router";
+        static_configs = [
+          {
+            targets = [ "klaus.lan.waldheinz.de:9100" ];
+          }
+        ];
       }
     ];
+
   };
 }

1461e0928921307c719db0e35a47397d13ab39b2
Matthias Treydte <mt@waldheinz.de>
2019-09-28 08:52:44 +0200

    module/nfs-blackbox: clean up mounts

--- modules/nfs-blackbox.nix
+++ modules/nfs-blackbox.nix
@@ -1,17 +1,13 @@
 { config, pkgs, ... }:
 let
   bb = what: {
-    device = ("blackbox:/mnt/tank/" + what);
+    device = "blackbox:/mnt/tank/" + what;
     fsType = "nfs";
     options = [ "proto=tcp6" ];
   };
 in {
   fileSystems = {
-    "/mnt/bb/pr0n" = bb "private/pr0n";
     "/mnt/bb/incoming" = bb "incoming";
-    "/mnt/bb/filme" = bb "media/Filme";
-    "/mnt/bb/serien" = bb "media/Serien";
-    "/mnt/bb/musik" = bb "media/Musik";
-    "/mnt/bb/fotos" = bb "private/Fotos";
+    "/mnt/bb/media" = bb "media";
   };
 }

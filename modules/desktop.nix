78778f7f4e1afd42b6246adc208833d0968924a4
Matthias Treydte <mt@waldheinz.de>
2019-09-29 15:52:24 +0200

    module/desktop: improve GTK apps under KDE plus misc. tools

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -2,11 +2,16 @@
 
 {
   environment.systemPackages = with pkgs; [
+    breeze-gtk
     chromium
     digikam
+    ffmpeg
     filelight
     firefox
     hexchat
+    kde-gtk-config
     mpv
+    transmission-remote-gtk
+    vlc
   ];
 }

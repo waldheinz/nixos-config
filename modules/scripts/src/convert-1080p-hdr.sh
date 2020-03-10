b24e6d45ad68d83866cf54acd25d0ac4fe5fefd9
Matthias Treydte <mt@waldheinz.de>
2020-03-10 07:32:40 +0100

    module/scripts/convert: fix totally broken script

--- modules/scripts/src/convert-1080p-hdr.sh
+++ modules/scripts/src/convert-1080p-hdr.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
+set -eou pipefail
+
 source="$1"
 
-function probe {
-    return $(ffprobe -v error -select_streams v:0 -show_entries stream="$1" \
-        -of default=noprint_wrappers=1:nokey=1 "$source")
+function probe() {
+    ffprobe \
+        -v error -select_streams v:0 -show_entries stream="$1" \
+        -of default=noprint_wrappers=1:nokey=1 "$source"
 }
 
 cspace=$(probe "color_space")
-ctrans=$(probe "bt2020nc")
-cprim=$(probe "bt2020nc")
+ctrans=$(probe "color_transfer")
+cprim=$(probe "color_primaries")
 x265_color="colorprim=$cprim:transfer=$ctrans:colormatrix=$cspace"
 
 ffmpeg \
     -i "$source" \
     -vf scale=-2:1080:flags=lanczos \
-    -c:v libx265 -preset slow -x265-params "crf=16:$x265_color" \
+    -c:v libx265 -preset slow -x265-params "crf=18:$x265_color" \
     -c:a libfdk_aac -strict experimental \
     "$source.mkv"

0c6da45f39b9bae67081693e6c40e694d4bdb26d
Matthias Treydte <mt@waldheinz.de>
2020-04-03 16:18:20 +0200

    module/work: update some vscode extensions

--- modules/work.nix
+++ modules/work.nix
@@ -9,21 +9,21 @@ let
         {
           publisher = "waderyan";
           name = "gitblame";
-          version = "3.0.1";
+          version = "3.2.0";
           sha256 = "1h68fzm9glv7gqwbi15sk6iw45kp9r08wrv2vd6lwi45srriwgjp";
         }
 
         {
           publisher = "timonwong";
           name = "shellcheck";
-          version = "0.8.1";
+          version = "0.9.0";
           sha256 = "0zg7ihwkxg0da0wvqcy9vqp6pyjignilsg9cldp5pp9s0in561cw";
         }
 
         {
           publisher = "ms-vscode";
           name = "vscode-typescript-tslint-plugin";
-          version = "1.2.2";
+          version = "1.2.3";
           sha256 = "1n2yv37ljaadp84iipv7czzs32dbs4q2vmb98l3z0aan5w2g8x3z";
         }
 
@@ -37,14 +37,14 @@ let
         {
           publisher = "justusadam";
           name = "language-haskell";
-          version = "2.6.0";
+          version = "2.7.0";
           sha256 = "1891pg4x5qkh151pylvn93c4plqw6vgasa4g40jbma5xzq8pygr4";
         }
 
         {
           publisher = "zxh404";
           name = "vscode-proto3";
-          version = "0.3.0";
+          version = "0.4.2";
           sha256 = "1iylw9hihqz0pab4iisykgrq20141v5f1r6l4cif1z4237nd3z60";
         }
       ];
@@ -55,7 +55,7 @@ in {
     gdb
     git
     git-lfs
-    haskellPackages.stylish-haskell
+    # haskellPackages.stylish-haskell
     my-vscode
     nodePackages.typescript
     shellcheck

8077c60f0c4159edfbf6e06d5225e6270534602b
Matthias Treydte <mt@waldheinz.de>
2020-04-03 10:09:23 +0200

    module/i18n: update for 20.03

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -1,12 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  i18n = {
-    consoleFont = "lat9w-16";
-    consoleKeyMap = "de";
-    defaultLocale = "de_DE.UTF-8";
-  };
-
+  i18n = { defaultLocale = "de_DE.UTF-8"; };
+  console = { font = "lat9w-16"; keyMap = "de"; };
   time.timeZone = "Europe/Berlin";
 
   environment.systemPackages = with pkgs; [

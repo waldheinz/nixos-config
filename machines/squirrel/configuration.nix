d449579ed2592982d778394fd998adf80d0dd6ce
Matthias Treydte <mt@waldheinz.de>
2020-11-19 10:30:58 +0100

    machine/bambi: disable PCIe ASPM (SSD does not like it)

--- machines/squirrel/configuration.nix
+++ machines/squirrel/configuration.nix
@@ -32,6 +32,7 @@
     };
   };
 
+  boot.kernelParams = [ "pcie_aspm=off" ];
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;

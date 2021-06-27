ca152768a64590cb795dc9779fd90f01c9def1b4
Matthias Treydte <mt@waldheinz.de>
2021-06-27 16:07:47 +0200

    machine/blackbox/minecraft: whitelist friends

--- machines/blackbox/minecraft.nix
+++ machines/blackbox/minecraft.nix
@@ -6,6 +6,9 @@
     eula = true;
     declarative = true;
     whitelist = {
+      hannes = "3cea33a7-8cd1-4a65-aa56-99ca3f9eab38";
+      hannes2 = "f878657f-1ddb-4eca-96ff-29d9f5beb2c2";  # Piet356
+      noschxl = "a8b58857-a15c-46ad-a017-54db27bc3652";
       waldheinz = "8cab3f52-270f-4c0d-a9fe-94c32d861201";
     };
 

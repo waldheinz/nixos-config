11eeddb6aa771abc2b958fd055ce09c98c6f4a09
Matthias Treydte <mt@waldheinz.de>
2021-12-08 11:11:18 +0100

    machine/homepi/transmission: add hostname to whitelist
    Since transmission now sees the hostname because
    nginx sets the header, this is required.

--- machines/homepi/transmission.nix
+++ machines/homepi/transmission.nix
@@ -11,6 +11,7 @@
       peer-port-random-high = 65500;
       peer-port-random-low = 65300;
       peer-port-random-on-start = true;
+      rpc-host-whitelist = "homepi.lan.waldheinz.de";
     };
   };
 

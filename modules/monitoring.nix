1b48ac6ef32843238d352290761f1f6851f3f889
Matthias Treydte <mt@waldheinz.de>
2021-12-08 10:09:28 +0100

    module/monitoring: add config option for textfile collector dir

--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -1,7 +1,25 @@
 { config, pkgs, ... }:
 
-{
-  services.prometheus.exporters = {
-    node.enable = true;
+let
+  textfiles-dir = config.local.node-exporter-textfiles;
+in {
+
+  options.local.node-exporter-textfiles = with pkgs.lib; mkOption {
+    type = types.str;
+    default = "/run/node-exporter-textfiles";
+    description = "Where node-exporter finds extra textfiles.";
+  };
+
+  config = {
+    systemd.tmpfiles.rules = [ "d ${textfiles-dir} 1777 root root" ];
+
+    services.prometheus.exporters.node = {
+      enable = true;
+      extraFlags = [
+          "--collector.textfile.directory ${textfiles-dir}"
+          "--collector.systemd.unit-exclude \".*\""  # we need no detailed per-unit metrics, nothing interesting there
+          "--web.disable-exporter-metrics"
+        ];
+    };
   };
 }

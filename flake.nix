ac2ec1161008f002c03550e7240a608dc9ef1af1
Matthias Treydte <mt@waldheinz.de>
2021-12-09 19:27:02 +0100

    machine/gehirnfasching: update to 21.11 and flakify

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
   outputs = { self, nixpkgs }: {
     nixosConfigurations = {
       blackbox = nixpkgs.lib.nixosSystem { system = "x86_64-linux"; modules = [ ./machines/blackbox ]; };
+      gehirnfasching = nixpkgs.lib.nixosSystem { system = "x86_64-linux"; modules = [ ./machines/gehirnfasching ]; };
       homepi = nixpkgs.lib.nixosSystem { system = "aarch64-linux"; modules = [ ./machines/homepi ]; };
       squirrel = nixpkgs.lib.nixosSystem { system = "x86_64-linux"; modules = [ ./machines/squirrel ]; };
     };

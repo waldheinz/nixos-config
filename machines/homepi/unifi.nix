84d661ae463743d6875822107925af24f55bd226
Matthias Treydte <mt@waldheinz.de>
2021-12-12 22:47:01 +0100

    machine/homepi/unifi: wait for unifi to be ready before starting the Prometheus exporter

--- machines/homepi/unifi.nix
+++ machines/homepi/unifi.nix
@@ -16,4 +16,19 @@
     unifiPassword = "pass";
   };
 
+  systemd.services.unifi-available = {
+    description = "Wait for Unifi to be available";
+
+    after = [ "unifi.service" ];
+    before = [ "prometheus-unifi-exporter.service" ];
+    wantedBy = [ "prometheus-unifi-exporter.service" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.curl}/bin/curl --insecure 'https://localhost:8443/'";
+      Restart = "on-failure";
+      RestartSec = "10";
+      Type = "oneshot";
+    };
+  };
+
 }

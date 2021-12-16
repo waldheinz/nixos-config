91baf726dd07fcdde2973e14791df6cdcf37e215
Matthias Treydte <mt@waldheinz.de>
2021-12-16 11:45:45 +0100

    machine/blackbox/storage: add service to tune HDD spin down during boot (seems not to be sticky)

--- machines/blackbox/storage-tank.nix
+++ machines/blackbox/storage-tank.nix
@@ -1,6 +1,19 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-{
+let
+  devices = [
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4E3TV59KV"
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4E4ZANL7A"
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4E6FT69XL"
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4E7JZVX8D"
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4EFHCFD5Y"
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4EHCEAP1H"
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4EKDH4SJ6"
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4EKTN9PKV"
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4ENNRS9V3"
+    "ata-WDC_WD40EFRX-68WT0N0_WD-WCC4EPA9U4C6"
+  ];
+in {
 
   fileSystems."/mnt/tank/incoming" =
     { device = "tank/incoming";
@@ -29,25 +42,41 @@
 
   environment.systemPackages = with pkgs; [ hdparm smartmontools ];
 
-  systemd.services.prometheus-node-exporter-smart = {
-    description = "Provide SMART data to Prometheus node exporter";
+  systemd = {
+    services.zpool-tank-hdparm = {
+      description = "Tune hdparm for zfs pool tank";
+      wantedBy = [ "multi-user.target" ];
 
-    serviceConfig = {
-      ExecStart = pkgs.writeShellScript "update-prometheus-node-smart" ''
-        set -euo pipefail
+      serviceConfig = {
+        ExecStart = pkgs.writeShellScript "zpool-tank-hdparm" ''
+          set -euo pipefail
 
-        ${pkgs.node-exporter-textfile-collector-scripts}/bin/smartmon.sh \
-          | ${pkgs.moreutils}/bin/sponge ${config.local.node-exporter-textfiles}/smartmon.prom
-      '';
+          ${lib.concatMapStrings (d: pkgs.hdparm + "/bin/hdparm -S 244 /dev/disk/by-id/" + d + "\n") devices}
+        '';
+
+        Type = "oneshot";
+      };
+    };
+
+    services.prometheus-node-exporter-smart = {
+      description = "Provide SMART data to Prometheus node exporter";
+
+      serviceConfig = {
+        ExecStart = pkgs.writeShellScript "update-prometheus-node-smart" ''
+          set -euo pipefail
+
+          ${pkgs.node-exporter-textfile-collector-scripts}/bin/smartmon.sh \
+            | ${pkgs.moreutils}/bin/sponge ${config.local.node-exporter-textfiles}/smartmon.prom
+        '';
+      };
+    };
+
+    timers.prometheus-node-exporter-smart = {
+      wantedBy = [ "prometheus-node-exporter.service" ];
+      timerConfig = {
+        OnBootSec = "1min";
+        OnUnitActiveSec = "2h10m";
+      };
     };
   };
-
-  systemd.timers.prometheus-node-exporter-smart = {
-    wantedBy = [ "prometheus-node-exporter.service" ];
-    timerConfig = {
-      OnBootSec = "1min";
-      OnUnitActiveSec = "1h10m";
-    };
-  };
-
 }
